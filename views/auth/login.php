926d2864d3669f0a63b62bf9ad542ec8604d0f3c
Nimur Islam Joy <sharierdigonto@gmail.com>
2026-01-19 23:02:42 +0600

    Update login.php

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,27 @@
 <?php
+ob_start();
+?>
+<from class="auth-form" method="post" action="index.php?action=login">
+    <div class="from-group">
+        <label for for="email">Email</label>
+        <input type="email" id="email" name="email" required>
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Login</button>
+    <p class="auth-switch">
+        <a href="index.php?action=forgotPassword">Forgot Password?</a>
+    </p>
+    <p class="auth-switch">
+        Don't have an account?
+        <a href="index.php?action=register">Create one</a>
+    </p>
+    
+
+</from>
+<?php
+$content = ob_clean();
+require __DIR__ . '/layout.php';
+?>
