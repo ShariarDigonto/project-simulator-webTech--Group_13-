5d6475db1b7badfd6b5100232040f504f5ceefae
nur672273-tech <nur672273@gmail.com>
2026-01-20 00:26:16 +0600

    Create layout.php

---
+++ views/admin/layout.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo isset($pageTitle) ? htmlspecialchars($pageTitle) . ' | ' : ''; ?>Admin Panel</title>
+    <link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
