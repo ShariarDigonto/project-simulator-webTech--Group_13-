99886c2f3912c52ebcc095d11c949fec680b009f
Parvezps7 <113965459+Parvezps7@users.noreply.github.com>
2026-01-19 23:48:16 +0600

    Create layout.php

---
+++ views/student/layout.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo isset($pageTitle) ? htmlspecialchars($pageTitle) . ' | ' : ''; ?>Student Panel</title>
+    <link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
