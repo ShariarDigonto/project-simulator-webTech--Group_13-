8836f2f24c80276a3af34b1c0fdaa2474936d533
Parvezps7 <113965459+Parvezps7@users.noreply.github.com>
2026-01-20 02:59:50 +0600

    Update assignments.php

--- views/student/assignments.php
+++ views/student/assignments.php
@@ -17,3 +17,16 @@
         <?php else: ?>
             <?php foreach ($assignments as $a): ?>
                 <tr>
+                    <td><?php echo htmlspecialchars($a['title']); ?></td>
+                    <td><?php echo htmlspecialchars($a['due_at'] ?? '-'); ?></td>
+                    <td><?php echo (int)$a['allow_late'] === 1 ? 'Yes' : 'No'; ?></td>
+                    <td>
+                        <?php if (!empty($a['file_path'])): ?>
+                            <a href="<?php echo htmlspecialchars($a['file_path']); ?>" target="_blank">Download</a>
+                        <?php else: ?>
+                            -
+                        <?php endif; ?>
+                    </td>
+                    <td>
+                        <a href="index.php?controller=StudentController&action=submit&assignment_id=<?php echo $a['id']; ?>">Submit</a>
+                    </td>
