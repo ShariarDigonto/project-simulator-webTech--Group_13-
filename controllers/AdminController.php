e8b53b374966f43791a9d755304fe224eab2c13b
nur672273-tech <nur672273@gmail.com>
2026-01-21 00:12:31 +0600

    Update AdminController.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -13,3 +13,11 @@ class AdminController
     public function dashboard()
     {
         $this->requireAdmin();
+         $pageTitle = 'Admin Dashboard';
+        $pendingCount = count(User::allPending());
+        $content = '<p>Welcome, Admin.</p>';
+        $content .= '<p><a href="index.php?controller=AdminController&action=users">Manage All Users</a></p>';
+        $content .= '<p><a href="index.php?controller=AdminController&action=pending">Pending Approvals (' . $pendingCount . ')</a></p>';
+        $content .= '<p><a href="index.php?action=logout">Logout</a></p>';
+        require __DIR__ . '/../views/admin/layout.php';
+    }
